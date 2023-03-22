f94913237b753b2d2f0eafbf44b6e3ddcb65fcbc
Luis Salcedo <luis.e.ss@gmail.com>
2023-03-21 18:05:09 -0600

    complete first-class function lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = (names) => [...names].slice(0, 2);
+
+const returnLastTwoDrivers = (drives) =>
+  drives.slice(drives.length - 2, drives.length);
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(fare) {
+  return (fare) => fare ** 2;
+}
+
+const fareDoubler = (fare) => fare * 2;
+
+const fareTripler = (fare) => fare * 3;
+
+function selectDifferentDrivers(drivers, returnFirstTwoDrivers) {
+  return returnFirstTwoDrivers(drivers);
+}
